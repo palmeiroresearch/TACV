fa596a57d256e744e46c7d83ca0ea349fc384f2b
palmeiroresearch <palmeiro.research@gmail.com>
2026-06-09 11:56:12 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    - Message: SKIP_WAITING para activar update aprobado por usuario
    ============================================================ */
 
-const CACHE_VERSION = 'v1.4.5';
+const CACHE_VERSION = 'v1.4.6';
 const CACHE_NAME    = `tac-viewer-${CACHE_VERSION}`;
 
 const ASSETS = [
